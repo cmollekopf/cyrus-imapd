cmollekopf/cyrus-imapd

commit 31d612831f719cfa288e7e03e51ff553bc9c6a50

imapd.c: always create the entire branch of mailboxes leading to a leaf

imap/imapd.c
@@ -7079,8 +7079,7 @@ static void cmd_create(char *tag, char *name, struct dlist *extargs, int localon
     }
 
 localcreate:
-#if 0  /* XXX  Do we want a config option for this?
-          XXX  Regardless, will will have to update Cassandane tests */
+    // XXX  Do we want a config option for this?
     // find the nearest ancestor to see if we have to fill out the branch
     r = mboxlist_findparent(mbname_intname(mbname), &parent);
     if (r == IMAP_MAILBOX_NONEXISTENT) r = 0;
@@ -7105,6 +7104,14 @@ localcreate:
             for (i = oldest; !r && i < youngest; i++) {
                 // create the ancestors
                 mbname_push_boxes(ancestor, strarray_nth(boxes, i));
+
+                /* Don't create any magic user.foo.INBOX mailboxes */
+                if (mbname_userid(ancestor) != NULL &&
+                    strarray_size(mbname_boxes(ancestor)) == 1 &&
+                    !strcmp(strarray_nth(mbname_boxes(ancestor), 0), "INBOX")) {
+                    continue;
+                }
+
                 r = mboxlist_createmailbox(mbname_intname(ancestor),
                                            mbtype, partition,
                                            imapd_userisadmin
@@ -7124,7 +7131,7 @@ localcreate:
         prot_printf(imapd_out, "%s NO %s\r\n", tag, error_message(r));
         goto done;
     }
-#endif
+
 
     // now create the requested mailbox
     r = mboxlist_createmailbox_opts(

commit b7e92363efd8955e9abb0300805d28cf6ec06588

Accept existing ancestors to new mailboxes
I've been running into this when creating multiple shared mailboxes,
such as shared/Resources/test1@kolab.org, shared/Resources/test2@kolab.org.
After the first mailbox, any subsequent mailbox creation with an @ in
the name would fail (because it tried to recreate the shared mailbox).

The regression was introduced by 31d612831f719cfa288e7e03e51ff553bc9c6a50,
and only affects the localcreate path (seems to work fine in a murder)

imap/imapd.c
@@ -7150,6 +7150,12 @@ localcreate:
                                            imapd_userid, imapd_authstate,
                                            flags, NULL/*mailboxptr*/);
                 if (r) {
+                    // If the mailbox already exists this should be fine.
+                    // TODO: check acl?
+                    if (r == IMAP_MAILBOX_EXISTS) {
+                        r = 0;
+                        continue;
+                    }
                     // XXX  should we delete the ancestors we just created?
                     break;
                 }