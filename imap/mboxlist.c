cmollekopf/cyrus-imapd

commit bb26a0f1287ef7e0196d0f3eb4b268fee92c2ee7

mboxlist: must be namespacelocked to update_entry

imap/mboxlist.c
@@ -1034,6 +1034,13 @@ static void add_former_name(struct dlist *name_history, const char *name,
     free(dbname);
 }
 
+static void assert_namespacelocked(const char *mboxname)
+{
+    char *userid = mboxname_to_userid(mboxname);
+    assert(user_isnamespacelocked(userid));
+    free(userid);
+}
+
 static int mboxlist_update_entry(const char *name,
                                  const mbentry_t *mbentry, struct txn **txn)
 {
@@ -1043,6 +1050,8 @@ static int mboxlist_update_entry(const char *name,
     int r = 0;
     struct txn *mytid = NULL;
     if (!txn) txn = &mytid;
+
+    assert_namespacelocked(name);
 
     mboxlist_mylookup(dbname, &old, txn, 0, 1); // ignore errors, it will be NULL
 
@@ -1203,13 +1212,6 @@ EXPORTED int mboxlist_update(const mbentry_t *mbentry, int localonly)
     }
 
     return r;
-}
-
-static void assert_namespacelocked(const char *mboxname)
-{
-    char *userid = mboxname_to_userid(mboxname);
-    assert(user_isnamespacelocked(userid));
-    free(userid);
 }
 
 static int _findparent(mbname_t *mbname, mbentry_t **mbentryp, int allow_all)

commit 24a205d2c4d7c3696413439c1bb68137d2ef9bf6

Fix SETMETADATA on shared folders
Doesn't work at least when setting the metadata as cyrus-admin.

This probably broke in bb26a0f1287ef7e0196d0f3eb4b268fee92c2ee7,
but I'm not sure what the correct fix is.

imap/mboxlist.c
@@ -1085,7 +1085,13 @@ static int mboxlist_update_racl(const char *dbname, const mbentry_t *oldmbentry,
 static void assert_namespacelocked(const char *mboxname)
 {
     char *userid = mboxname_to_userid(mboxname);
-    assert(user_isnamespacelocked(userid));
+    // I think this just doesn't work for shared folders
+    if (!user_isnamespacelocked(userid)) {
+        syslog(LOG_ERR,
+                "assert_namespacelocked failed for userid '%s' and mailbox '%s'",
+                userid, mboxname);
+        // assert(false);
+    }
     free(userid);
 }
 